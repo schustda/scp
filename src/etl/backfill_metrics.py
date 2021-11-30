b596d085c0dc6ae1e3ee7768195bb14477febd9f
Douglas Schuster <douglas.schuster303@gmail.com>
2021-11-30 11:30:43 -0500

    up the quota

--- src/etl/backfill_metrics.py
+++ src/etl/backfill_metrics.py
@@ -30,7 +30,7 @@ def metrics_production():
         FROM market.price_history
         WHERE one_wk_avg IS NULL
         AND date < '2021-11-29'
-        LIMIT 10
+        LIMIT 100
     ''')
     for ticker in tickers:
         with ps.conn.connect() as con:

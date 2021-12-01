1ff2a21c00913cdd04985b7d67542b1666c337c1
Douglas Schuster <douglas.schuster303@gmail.com>
2021-11-30 19:18:19 -0500

    only execute one query

--- src/etl/backfill_metrics.py
+++ src/etl/backfill_metrics.py
@@ -32,26 +32,25 @@ def metrics_production():
         AND date < '2021-11-29'
         LIMIT 100
     ''')
-    for ticker in tickers:
-        with ps.conn.connect() as con:
-            con.execute(f'''
-                with cte AS (
-                    SELECT date, ticker
-                        ,AVG(ohlc) OVER(ORDER BY date ROWS BETWEEN 1 FOLLOWING AND 5 FOLLOWING) AS one_wk_avg
-                        ,AVG(ohlc) OVER(ORDER BY date ROWS BETWEEN 1 FOLLOWING AND 10 FOLLOWING) AS two_wk_avg
-                        ,AVG(dollar_volume) OVER(ORDER BY date ROWS BETWEEN CURRENT ROW AND 10 FOLLOWING) AS two_wk_vol 
-                    FROM market.price_history
-                    WHERE ticker = '{ticker}'
-                )        
-                UPDATE market.price_history ph
-                SET one_wk_avg =  cte.one_wk_avg
-                    ,two_wk_avg = cte.two_wk_avg
-                    ,two_wk_vol = cte.two_wk_vol
-                FROM cte
-                WHERE ph.date = cte.date
-                AND ph.ticker = cte.ticker;
-                COMMIT;
-        ''')
+    with ps.conn.connect() as con:
+        con.execute(f'''
+            with cte AS (
+                SELECT date, ticker
+                    ,AVG(ohlc) OVER(ORDER BY date ROWS BETWEEN 1 FOLLOWING AND 5 FOLLOWING) AS one_wk_avg
+                    ,AVG(ohlc) OVER(ORDER BY date ROWS BETWEEN 1 FOLLOWING AND 10 FOLLOWING) AS two_wk_avg
+                    ,AVG(dollar_volume) OVER(ORDER BY date ROWS BETWEEN CURRENT ROW AND 10 FOLLOWING) AS two_wk_vol 
+                FROM market.price_history
+                WHERE ticker IN ('{"','".join(tickers)}')
+            )        
+            UPDATE market.price_history ph
+            SET one_wk_avg =  cte.one_wk_avg
+                ,two_wk_avg = cte.two_wk_avg
+                ,two_wk_vol = cte.two_wk_vol
+            FROM cte
+            WHERE ph.date = cte.date
+            AND ph.ticker = cte.ticker;
+            COMMIT;
+    ''')
     return
 
 

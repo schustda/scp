b1dfe0c9357964c7d7da4ee8b12decaf901f141d
Douglas Schuster <douglas.schuster303@gmail.com>
2021-12-06 14:10:13 -0500

    removing the update statement

--- src/etl/aggregate.py
+++ src/etl/aggregate.py
@@ -28,33 +28,19 @@ def insert():
     with ps.conn.connect() as con:
         con.execute('''
     with ih AS (
-        SELECT 
-            CAST(ms.message_date AS date) date
-            ,ticker
-            ,AVG(sentiment_polarity) sentiment_polarity
-            ,AVG(sentiment_subjectivity) sentiment_subjectivity
-            ,COUNT(*) posts
-        FROM ihub.message_sentiment ms
-        LEFT JOIN items.symbol s ON ms.ihub_code = s.ihub_code
-        WHERE exchange = 'usotc'
-        GROUP BY CAST(ms.message_date AS date), ticker
+        SELECT date, ticker, sentiment_polarity, sentiment_subjectivity, posts, RANK() OVER(PARTITION BY date ORDER BY posts DESC) daily_ranking
+        FROM (
+            SELECT CAST(ms.message_date AS date) date, ticker, AVG(sentiment_polarity) sentiment_polarity, AVG(sentiment_subjectivity) sentiment_subjectivity, COUNT(*) posts
+            FROM ihub.message_sentiment ms
+            LEFT JOIN items.symbol s ON ms.ihub_code = s.ihub_code
+            WHERE exchange = 'usotc'
+            GROUP BY CAST(ms.message_date AS date), ticker
+        ) x
     )
-    
-    INSERT INTO ihub.board_date (date, ticker, sentiment_polarity, sentiment_subjectivity, posts, ohlc, dollar_volume, one_wk_avg, two_wk_avg, two_wk_vol, target)
-    SELECT 
-        ph.date
-        ,ph.ticker
-        ,sentiment_polarity
-        ,sentiment_subjectivity
-        ,posts
-        ,ohlc
-        ,dollar_volume
-        ,one_wk_avg
-        ,two_wk_avg
-        ,two_wk_vol
-        ,target
+
+    INSERT INTO ihub.board_date (date, ticker, sentiment_polarity, sentiment_subjectivity, posts, daily_ranking, ohlc, dollar_volume, one_wk_avg, two_wk_avg, two_wk_vol, target)
+    SELECT ph.date,ph.ticker,sentiment_polarity,sentiment_subjectivity,posts,daily_ranking,ohlc,dollar_volume,one_wk_avg,two_wk_avg,two_wk_vol,target
     FROM market.price_history ph
-        
     LEFT JOIN ih
     ON ph.ticker = ih.ticker
     AND ph.date = ih.date;
@@ -67,24 +53,6 @@ def truncate():
     ps = PSQL('scp')
     with ps.conn.connect() as con:
         con.execute('''TRUNCATE TABLE ihub.board_date; COMMIT;''')
-    return
-
-def update():
-    ps = PSQL('scp')
-    with ps.conn.connect() as con:
-        con.execute('''
-    with cte AS (
-        SELECT date, ticker, RANK() OVER(PARTITION BY date ORDER BY posts DESC) daily_ranking
-        FROM ihub.board_date
-    )
-
-    UPDATE ihub.board_date bd
-    SET daily_ranking = cte.daily_ranking
-    FROM cte
-    WHERE bd.date = cte.date
-    AND bd.ticker = cte.ticker;
-    COMMIT;
-    ''')
     return
     
 def define_target():
@@ -144,13 +112,8 @@ python_callable=insert,
 dag=dag)
 
 t3 = PythonOperator(
-task_id='update',
-python_callable=update,
-dag=dag)
-
-t4 = PythonOperator(
 task_id='define_target',
 python_callable=define_target,
 dag=dag)
 
-t1 >> t2 >> t3 >> t4
+t1 >> t2 >> t3

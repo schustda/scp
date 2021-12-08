682c4c53f03664fa6cf7964a9222a37b25eaf9ae
Douglas Schuster <douglas.schuster303@gmail.com>
2021-12-08 09:20:17 -0500

    break out data setup a little bit

--- src/model/data_setup.py
+++ src/model/data_setup.py
@@ -22,26 +22,40 @@ default_args = {
     'schedule_interval': '@daily',
 }
 
+def create_staging_tables():
+    ps = PSQL('scp')
+    with ps.conn.connect() as con:
+        for tbl_name in ['idx1','idx2','idx3']:
+            con.execute(f'''CREATE TABLE staging.{tbl_name} (idx int);
+            COMMIT;''')
+    return
+
+def fill_staging_table1():
+    ps = PSQL('scp')
+    with ps.conn.connect() as con:
+        con.execute(f'''INSERT INTO staging.idx1 (idx) SELECT idx FROM ihub.board_date WHERE target IS NOT NULL;
+        COMMIT;''')
+    return
+
+def fill_staging_table2():
+    ps = PSQL('scp')
+    with ps.conn.connect() as con:
+        con.execute(f'''INSERT INTO staging.idx2 (idx) SELECT idx FROM staging.idx1 WHERE target IS NOT NULL;
+        COMMIT;''')
+    return
+
+def fill_staging_table3():
+    ps = PSQL('scp')
+    with ps.conn.connect() as con:
+        con.execute(f'''INSERT INTO staging.idx3 (idx) SELECT idx FROM staging.idx2 WHERE target IS NOT NULL;
+        COMMIT;''')
+    return
+
 def insert():
 
     ps = PSQL('scp')
     with ps.conn.connect() as con:
         con.execute('''
-    SELECT idx INTO model.idx1 FROM ihub.board_date WHERE target IS NOT NULL;
-    SELECT idx INTO model.idx2 FROM model.idx1 TABLESAMPLE SYSTEM (75);
-    SELECT idx INTO model.idx3 FROM model.idx2 TABLESAMPLE SYSTEM (75);
-
-    with wk_tr AS (
-        SELECT idx FROM ihub.board_date
-        TABLESAMPLE SYSTEM (75)
-        WHERE Target IS NOT NULL
-    )
-
-    ,md_tr AS (
-        SELECT idx FROM wk_tr
-    )
-
-
     INSERT INTO model.combined_data (idx,ohlc,dollar_volume,posts,sentiment_polarity,sentiment_subjectivity,daily_ranking,target,working_train,working_validation,model_development_train,model_development_test)
         
     SELECT
@@ -53,16 +67,12 @@ def insert():
     ,sentiment_subjectivity
     ,daily_ranking
     ,target
-    ,CASE WHEN idx IN (SELECT idx FROM model.idx2) THEN True ELSE False END AS working_train
-    ,CASE WHEN idx NOT IN (SELECT idx FROM model.idx2) THEN True ELSE False END AS working_validation
-    ,CASE WHEN idx IN (SELECT idx FROM model.idx3) THEN True ELSE False END AS model_development_train
-    ,CASE WHEN idx NOT IN (SELECT idx FROM model.idx3) THEN True ELSE False END AS model_development_test
+    ,CASE WHEN idx IN (SELECT idx FROM staging.idx2) THEN True ELSE False END AS working_train
+    ,CASE WHEN idx NOT IN (SELECT idx FROM staging.idx2) THEN True ELSE False END AS working_validation
+    ,CASE WHEN idx IN (SELECT idx FROM staging.idx3) THEN True ELSE False END AS model_development_train
+    ,CASE WHEN idx NOT IN (SELECT idx FROM staging.idx3) THEN True ELSE False END AS model_development_test
     FROM ihub.board_date
     WHERE target IS NOT NULL;
-
-    DROP TABLE model.idx1;
-    DROP TABLE model.idx2;
-    DROP TABLE model.idx3;
     COMMIT;
     ''')
     return
@@ -74,7 +84,12 @@ def truncate():
         con.execute('''TRUNCATE TABLE model.combined_data; COMMIT;''')
     return
     
-    
+def drop_staging_table():
+    ps = PSQL('scp')
+    with ps.conn.connect() as con:
+        for tbl_name in ['idx1','idx2','idx3']:
+            con.execute(f'''DROP TABLE staging.{tbl_name}; COMMIT;''')
+    return
 
 dag = DAG(
     dag_id='Model_Data_Setup', default_args=args,catchup=False)
@@ -85,8 +100,33 @@ python_callable=truncate,
 dag=dag)
 
 t2 = PythonOperator(
+task_id='create_staging_tables',
+python_callable=create_staging_tables,
+dag=dag)
+
+t3 = PythonOperator(
+task_id='fill_staging_table1',
+python_callable=fill_staging_table1,
+dag=dag)
+
+t4 = PythonOperator(
+task_id='fill_staging_table2',
+python_callable=fill_staging_table2,
+dag=dag)
+
+t5 = PythonOperator(
+task_id='fill_staging_table3',
+python_callable=fill_staging_table3,
+dag=dag)
+
+t6 = PythonOperator(
 task_id='insert',
 python_callable=insert,
 dag=dag)
 
-t1 >> t2
+t7 = PythonOperator(
+task_id='drop_staging_table',
+python_callable=drop_staging_table,
+dag=dag)
+
+t1 >> t2 >> t3 >> t4 >> t5 >> t6 >> t7
